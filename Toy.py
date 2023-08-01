a1b4ce8a81780bb2f3de4cbe83d25932c8a85478
Oslec102 <102330824+Oslec102@users.noreply.github.com>
2023-08-01 18:57:35 -0300

    Add files via upload

--- Toy.py
+++ Toy.py
@@ -46,16 +46,6 @@ print("======================================")
 print(Distancia_d)
 print()
 
-'''
-#Dicionário com os dias da semana.
-#D = ("Seg", "Ter", "Qua", "Qui", "Sex", "Sab", "Dom")
-D = pd.read_excel('ProblemaToy.xlsx',sheet_name=0, usecols=[4,5,6,7,8,9])
-print("======================================")
-print("| D                                  |")
-print("======================================")
-print(D)
-print()
-'''
 # Dicionário com os dias da semana.
 # D = ("Seg", "Ter", "Qua", "Qui", "Sex", "Sab", "Dom")
 D = pd.read_excel('ProblemaToy.xlsx', sheet_name=0, usecols=[4, 5, 6, 7, 8, 9])
@@ -74,14 +64,14 @@ h = {}
 for index, row in dadosminutos.iterrows():
     # Extrai as informações de colaborador, dia e turno
     colaborador = row['Colaborador']
-    dia = row['Dia']
+    d = row['Dia']
     turno = row['Turno']
     
     # Extrai a carga horária máxima
     carga_horaria = row['Carga Horaria Maxima']
     
     # Adiciona a carga horária máxima ao dicionário
-    h[(colaborador, dia, turno)] = carga_horaria
+    h[(colaborador, d, turno)] = carga_horaria
 
 # Exibe o dicionário
 print("======================================")
@@ -121,19 +111,19 @@ dias_semana = {
     'Sab': 'Sab'
 }
 
-# Criar o parâmetro ld[a, d, t] = número de vezes por dia que a área a tem que ser limpa no turno t
-ld = {}
+# Criar o parâmetro nc[a, d, t] = número de vezes por dia que a área a tem que ser limpa no turno t
+nc = {}
 for index, row in limpeza_df.iterrows():
-    area = row['Area']
+    a = row['Area']
     turno = row['Turno']
-    for dia, dia_abreviado in dias_semana.items():
-        ld[area, dia_abreviado, turno] = row[dia]
+    for d, dia_abreviado in dias_semana.items():
+        nc[a, dia_abreviado, turno] = row[d]
 
-# Exibir o parâmetro ld
+# Exibir o parâmetro nc
 print("======================================")
-print("| Parâmetro ld                       |")
+print("| Parâmetro nc                       |")
 print("======================================")
-for key, value in ld.items():
+for key, value in nc.items():
     print(f"{key}: {value}")
 print()
 
@@ -149,9 +139,9 @@ print(listaPredios)
 print()
 
 for index, row in df_predioArea.iterrows():
-    area = row['Area']
+    a = row['Area']
     predio = row['Predio']
-    ar[area] = predio
+    ar[a] = predio
 print("======================================")
 print("| ar                                  |")
 print("======================================")
@@ -161,12 +151,12 @@ print()
 def predios_diferentes(p1, p2):
     return 1 if ar[p1] != ar[p2] else 0
 
-f_df = pd.read_excel('ProblemaToy.xlsx',sheet_name=0, usecols=[10])
-f_df_clean = f_df.dropna()
+f_df = pd.read_excel('ProblemaToy.xlsx',sheet_name=0, usecols=[0,10])
+#f_df_clean = f_df.dropna()
 print("======================================")
 print("| f_df                                  |")
 print("======================================")
-print(f_df_clean)
+print(f_df)
 print()
 
 
@@ -180,7 +170,7 @@ num_predios = 3
 model = Model()
 
 # CRIAR VARIÁVEIS DE DECISÃO
-x = {(c, a): model.add_var(var_type=INTEGER,name="x({},{})".format(c, a)) for c in C_df['Colaborador'].values.tolist() for a in A_df['Area'].values.tolist()}
+x = {(c, a, d, t): model.add_var(var_type=BINARY,name="x({},{},{},{})".format(c, a, d, t)) for c in C_df['Colaborador'].values.tolist() for a in A_df['Area'].values.tolist()  for d, dia_abreviado in dias_semana.items() for t in T_df['Turno'].values.tolist()}
 
 # Criar variável U
 u = {(c, p1, p2): model.add_var(var_type=BINARY, name="u({},{},{})".format(c, p1, p2)) for c in C_df['Colaborador'].values.tolist() for p1 in listaPredios for p2 in listaPredios}
@@ -192,33 +182,40 @@ model.objective = xsum(u[c, p1, p2] * Distancia_d.loc[p1, p2] for c in C_df['Col
 
 #Cada área deve ser alocada ao número requerido de colaboradores em cada turno e dia da semana
 for a in A_df['Area'].values.tolist():
-    for dia, dia_abreviado in dias_semana.items():
+    for d, dia_abreviado in dias_semana.items():
         for t in T_df['Turno'].values.tolist():
-            if (a, dia, t) in ld:   
-                model  += xsum(x[(c,a)] for c in C_df['Colaborador'].values.tolist()) == ld[(a,dia,t)]
+            if (a, d, t) in nc:   
+                model  += xsum(x[(c,a,d,t)] for c in C_df['Colaborador'].values.tolist()) == nc[(a,d,t)]
+            else:
+                model += xsum(x[c,a,d,t] for c in C_df['Colaborador'].values.tolist()) == 0
 
 
 #A soma dos tempos de limpeza e deslocamentos não pode exceder a carga horária diária por turno de cada colaborador
-for dia, dia_abreviado in dias_semana.items():
-    for t in T_df['Turno'].values.tolist():
-        for p1 in listaPredios:
-            for p2 in listaPredios:
-                for d in Distancia_d:
-                    for c in C_df['Colaborador'].values.tolist():
-                        if (colaborador, dia, turno) in h:
-                            model += xsum(x[(c,a)] for c in C_df['Colaborador'].values.tolist() * t[(a)] * Distancia_d.loc[p1, p2]) <= h[(colaborador, dia, turno)]
+for d, dia_abreviado in dias_semana.items():
+        for t in T_df['Turno'].values.tolist():
+                for c in C_df['Colaborador'].values.tolist():
+                    if (c, d, t) in h:
+                        model += xsum(x[(c,a,d,t)] * t_a[a] for a in t_a) <= h[(c, d, t)]
+                    
 
-  #Tem que considerar o parametro Distancia na soma?
+
+
+                #Tem que considerar o parametro Distancia na soma?
                 
 
-# Respeitar as fixações de colaborador para área quando determinado
-for c in C_df['Colaborador'].values.tolist():
-    for a in A_df['Area'].values.tolist():
-        if (c, a) in f_df_clean:
-            f_fixo = f_df_clean.loc[(c, a)].values[0]
-            model += x[(c, a)] >= f_fixo
+#Respeitar as fixações de colaborador para área quando determinado
+# for index, row in f_df.iterrows():
+#      for d, dia_abreviado in dias_semana.items():
+#         for t in T_df['Turno'].values.tolist():
+#             c = row['ColaboradorFixo']
+#             a = row['Area']
+#             if str(c) != "nan":
+#                 if (a,d,t) in nc:
+#                     if nc[(a,d,t)] > 0:
+#                         for colaborador in c.split("+"):
+#                             model += x[(c,a,d,t)] >= 1 
 
-# RESOLVER O MODELO
+            # RESOLVER O MODELO
 model.optimize()
 model.write('teste.lp')
 
@@ -226,10 +223,12 @@ model.write('teste.lp')
 # IMPRIMIR RESULTADOS
 if model.status == OptimizationStatus.OPTIMAL:
     for c in C_df['Colaborador'].values.tolist():
-        for p1 in listaPredios:
-            for p2 in listaPredios:
-                if u[c, p1, p2].x >= 0.99:
-                    print(f"O colaborador {c} está designado para o prédio {p1}.")
+        for a in A_df['Area'].values.tolist():
+            for d, dia_abreviado in dias_semana.items():
+                for t in T_df['Turno'].values.tolist():
+
+                    if x[(c, a,d,t)].x >= 0.99:
+                        print(f"x({c}, {a}, {d}, {t}) = 1")
 else:
     print("A solução ótima não foi encontrada.")
 
@@ -243,3 +242,4 @@ print(model)
 
 
 
+#- nc[a, d, t] = número de colaboradores que limpam conjuntamente a area a no dia d no turno t
